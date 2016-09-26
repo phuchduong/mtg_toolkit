16213009fb09c13a014dee66d1ebe24c9437ee3e
Phuc Duong <phuchduong@datasciencedojo.com>
2016-09-25 23:05:32 -0700

    added csv to tcg

--- tcgplayer cart to csv/tcgplayer cart to csv.py
+++ tcgplayer cart to csv/tcgplayer cart to csv.py
@@ -1,10 +1,25 @@
 from bs4 import BeautifulSoup
 
 html_file = "C:/Users/phuc/Downloads/temp tcg cart/card.html"
-distributor_div = ["div", "class", "sellerWrapMarket"]
 
+# Classes
+card_n_set_class = "sellerWrapMarket"
+rarity_n_condition_class = "detailsContents"
+price_class = "priceBox"
+quantity_class = "qtyBox"
+
+# parses file
 raw_html = open(html_file, 'r').read()
 cart_soup = BeautifulSoup(raw_html, "html.parser")
 
-cart_soup.find(distributor_div[0], {distributor_div[1]: distributor_div[2]})
-cart_soup[0]
+# dictionary of cards
+card_dict = {}
+
+# Grab card and set
+= cart_soup.find_all("div", class_=card_n_set_class)
+= cart_soup.find_all("div", class_=rarity_n_condition_class)
+= cart_soup.find_all("div", class_=price_class)
+= cart_soup.find_all("div", class_=quantity_class)
+
+# loops through each distributor
+for distributor in distributors_sections:
