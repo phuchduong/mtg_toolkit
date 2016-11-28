b0a3297779f9afc44dc8bde7b0911362f85bf876
Phuc Duong <phuchduong@hotmail.com>
2016-11-28 08:56:36 -0800

    added python module tutorial

--- collection_management/exploration_and_experimentation/name_vs_keyname.py
+++ collection_management/exploration_and_experimentation/name_vs_keyname.py
@@ -1,13 +1,32 @@
 import os
+import json
 
-# Find MTG JSON DB
-# current_dir = os.path.dirname(os.path.realpath(__file__))
-dir_path =  
-card_db_name = "mtg_card_db.json"
+db_folder_name = "mtg_json"
+db_file_name = "mtg_card_db.json"
 
-# Ingress
-# mtg_db = json.loads(open(file_name).read())
-mtg_db = json.loads(
 
+# Reds a json file in a sister directory
+def load_json_file(folder_name, file_name):
+    # Find MTG JSON DB
+    # Your file structure should look like this
+    #
+    # + exploration_and_experimentation <-- current directory
+    # | -- name_vs_keyname.py           <-- current file
+    # + mtg_json                        <-- file dir
+    # | -- mtg_card_db.json             <-- target file
+    #
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    file_path = os.path.join(current_dir, "..", folder_name, file_name)
+
+    # Ingress
+    json_file = json.loads(open(file_path).read())
+
+    return(json_file)
+
+
+mtg_db = load_json_file(folder_name=db_folder_name, file_name=db_file_name)
+
+print(str(mtg_db.keys()).encode("utf-8"))
 # view the difference between the key and name
-def view_difference():
+#   def view_difference():
