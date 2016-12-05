28f4568a3696906020e8c7f82f9e342c0699aeb1
Phuc Duong <phuchduong@hotmail.com>
2016-12-05 08:31:20 -0800

    exploration script

--- collection_management/exploration.py
+++ collection_management/exploration.py
@@ -3,6 +3,17 @@ import mtg_db_manager as db
 # db.download_json_db()
 mtg_db = db.load_json_file()
 
-card_name = mtg_db.keys()
+card_names = mtg_db.keys()
 
-print(card_name)
+card_names = sorted(card_names)
+
+# building headers
+headers = []
+for card_name in card_names:
+    for header in mtg_db[card_name]:
+        if header not in headers:
+            headers.append(header)
+
+# writes file
+f.open
+for card_name in card_names:
