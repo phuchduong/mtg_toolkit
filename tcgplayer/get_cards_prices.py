717da41e105b73351bbb0f5c044b0b90b0fba0a0
Phuc Duong <phuchduong@hotmail.com>
2017-07-08 01:56:10 -0700

    finished python script to scrape a single page and write the entry out to two csv files

--- tcgplayer/get_cards_prices.py
+++ tcgplayer/get_cards_prices.py
@@ -7,50 +7,70 @@
 from bs4 import BeautifulSoup as soup       # html data structure
 from urllib.request import urlopen as uReq  # web client
 from urllib.request import quote            # encoder
+from urllib.request import Request          # encoder
 import pandas as pd                         # data frame structure
 from datetime import datetime               # clock
 
+# read in existing csv table
+df = pd.read_csv("tcg_prices.csv")
+before = len(df)        # last row index +1
+cur_index = len(df)     # last row index +1
+
+# spoofing a user agent
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36"
+
 ###################################
-# initialize data frame
+# read in list of sets
 ###################################
-df = pd.DataFrame(
-    columns=(
-        "card_name",
-        "mana_cost",
-        "set_name",
-        "rarity",
-        "price_h",
-        "price_m",
-        "price_l"
-    )
-)
-cur_index = 1
+sets = pd.read_csv("set_names.csv")
+sets = sets.fillna("")
+
+# finds the first "not done" set to scrape
+not_done = sets["status"] != "done"
+set_name = sets[not_done].loc[0]["set_name"]
+
+
+print("parsing... " + set_name)
+
 
 ###################################
 # form url
 ###################################
-set_name = "battle for zendikar"
 
-set_name = quote(set_name)
+set_name_url = quote(set_name)   # encode html escape characters
+
+root_url = "http://magic.tcgplayer.com/db/search_result.asp?Set_Name="
+
+set_url = root_url + set_name_url
+
+print("url: " + set_url)
+
+# spoof header
+req = Request(set_url)
+req.add_header("User-Agent", "Mozilla/5.0")
+
 
 ###################################
 # download and parse page into soup
 ###################################
 
-form_url = "http://magic.tcgplayer.com/db/search_result.asp?Set_Name=battle%20for%20zendikar"
-
 scrape_date = str(datetime.now())
-web_client = uReq(form_url)
+web_client = uReq(req)
+print("web_client: " + str(web_client.getcode()))
 
 soup = soup(web_client.read(), "html.parser")
+web_client.close()
+
 
 #######################
 # isolate pricing table
 #######################
+
 html_tables = soup.find_all("table")
 
 # lots of tables. We only need the pricing table
 html_table = html_tables[8]
+
 
 #######################
 # parse table
@@ -79,11 +99,25 @@ for row in rows:
         rarity,
         price_h,
         price_m,
-        price_l
+        price_l,
+        scrape_date
     ]
     cur_index += 1
 
-# add scrape date to each entry
-df["scrape_date"] = scrape_date
+# how many rows were added?
+diff = cur_index - before
+print("data size: +" + str(diff) + " rows added.")
 
+# writes to csv
 df.to_csv("tcg_prices.csv", index=False)
+
+##############################################
+# mark set as completed in set_names.csv file
+##############################################
+
+# grabs the first index, after the filter,
+# which is the actual index of the row
+act_index = sets[not_done].iloc[0].name
+sets = sets.set_value(act_index, "status", "done")
+print(set_name + " is marked as complete.")
+sets.to_csv("set_names.csv", index=False)
