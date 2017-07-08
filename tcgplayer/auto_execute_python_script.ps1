e5d19db32dafbfb8541848b16ea5d063bc515591
Phuc Duong <phuchduong@hotmail.com>
2017-07-08 02:10:10 -0700

    successfully executed the python script in a loop using powershell

--- tcgplayer/auto_execute_python_script.ps1
+++ tcgplayer/auto_execute_python_script.ps1
@@ -1,1 +1,7 @@
-python .\get_cards_prices.py
+$start = (Get-Date).Milisecond
+For ($i=0; $i -lt 195; $i++){
+    Write-Host $i
+    python .\get_cards_prices.py
+}
+$end = (Get-Date).Milisecond
+Write-Host "This script took $($end - $start) miliseconds to run."
