07ab1068e1884794a6230d7ea798a48879df8d4d
Phuc Duong <phuchduong@hotmail.com>
2017-07-08 23:40:33 -0700

    removed and merged duplicates from inventory sheet

--- merge_inv_with_tcg.R
+++ merge_inv_with_tcg.R
@@ -8,3 +8,11 @@ setwd("D:/repos/mtg_toolkit")
 inv = read.csv("inventory.csv", stringsAsFactors = FALSE)
 tcg = read.csv("tcgplayer/tcg_prices.csv", stringsAsFactors = FALSE)
 
+tcg$key <- tolower(tcg$card_name)
+tcg.single <- tcg[tcg$reprints==1,]
+tcg.reprint <- tcg[tcg$reprints!=1,]
+
+tcg2 = merge(x = tcg.single, y = inv, by.x = "key", by.y = "card_name", all.x = TRUE, all.y = FALSE)
+
+write.csv(tcg.single, file = "tcg_single.csv", row.names = FALSE)
+write.csv(tcg2, file = "tcg_merged.csv", row.names = FALSE)
