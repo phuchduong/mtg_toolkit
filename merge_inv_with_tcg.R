a96ca804403e7aeb1f81151a73e25c8c353c6fbc
Phuc Duong <phuchduong@hotmail.com>
2017-07-09 00:03:27 -0700

    merged tcgsheet with inventory.

--- merge_inv_with_tcg.R
+++ merge_inv_with_tcg.R
@@ -14,5 +14,31 @@ tcg.reprint <- tcg[tcg$reprints!=1,]
 
 tcg2 = merge(x = tcg.single, y = inv, by.x = "key", by.y = "card_name", all.x = TRUE, all.y = FALSE)
 
-write.csv(tcg.single, file = "tcg_single.csv", row.names = FALSE)
-write.csv(tcg2, file = "tcg_merged.csv", row.names = FALSE)
+# if the two rows do not match then that means there were duplications
+# in the inventory sheet. go and remove them.
+nrow(tcg2) == nrow(tcg.single)
+#write.csv(tcg.single, file = "tcg_single.csv", row.names = FALSE)
+#write.csv(tcg2, file = "tcg_merged.csv", row.names = FALSE)
+
+# merge the two tcg sheets back together
+tcg.reprint$nf_qty <- NA
+tcg.reprint$f_qty <- NA
+tcg.reprint$is_bulk <- NA
+tcg3 <- rbind(tcg2, tcg.reprint, stringsAsFactors=FALSE)
+
+# finds cards that have been reprinted, from inventory
+inv.reprinted <- inv[!inv$card_name %in% tcg.single$key,]
+
+# format the reprinted carsd to have same columns and unknown set
+inv.reprinted$mana_cost <- NA
+inv.reprinted$set_name <- "unknown set"
+inv.reprinted$rarity <- NA
+inv.reprinted$price_h <- NA
+inv.reprinted$price_m <- NA
+inv.reprinted$price_l <- NA
+inv.reprinted$scrape_date <- NA
+inv.reprinted$reprints <- NA
+tcg3$key <- NULL
+tcg4 <- rbind(tcg3, inv.reprinted, stringsAsFactors=FALSE)
+
+write.csv(x=tcg4, file="inventory 2017-07-09.csv", row.names = FALSE)
